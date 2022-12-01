12e37afa54d154acba6979e2ab95bf9561f93f5c
shanhaider23 <shan.haider@gmail.com>
2022-12-01 14:04:05 +0100

    updated project

---
+++ src/app.vue
@@ -0,0 +1,46 @@
+<template>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/Resources/StoredResources.vue';
+export default {
+  components: {
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'offical-guide',
+          title: 'Offical Guide',
+          description: 'The offical vue.js guide',
+          link: 'https://v2.vuejs.org/v2/style-guide/?redirect=true#Priority-A-Essential',
+        },
+        {
+          id: 'google',
+          title: 'Offical Guide',
+          description: 'The offical vue.js guide',
+          link: 'https://v2.vuejs.org/v2/style-guide/?redirect=true#Priority-A-Essential',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
 import { createApp } from 'vue';
-
-createApp({}).mount('#app');
+import App from './App.vue'
+createApp(App).mount('#app');
